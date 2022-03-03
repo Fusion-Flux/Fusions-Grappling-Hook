cadbb9bc081a499cec92f362c8387cd8eda5f53d
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2022-03-03 16:58:35 -0600

    Temp commit before i break everything

--- src/main/java/com/fusionflux/grapple/items/GrappleItem.java
+++ src/main/java/com/fusionflux/grapple/items/GrappleItem.java
@@ -1,16 +1,26 @@
 package com.fusionflux.grapple.items;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GrappleItem extends Item {
 
@@ -31,16 +41,37 @@ public class GrappleItem extends Item {
         if (!isHooked) {
             //Check to make sure the HitResult hits a block
             if (hitResult.getType() == HitResult.Type.BLOCK) {
+                //world.playSound(null, user.getPos().x,user.getPos().y,user.getPos().z, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.NEUTRAL, 1F, 1F);
 
                 //Gets the position of the HitResult
-                hitPos = ((BlockHitResult) hitResult).getPos();
+                hitPos = hitResult.getPos();
 
+                BlockSoundGroup blockSoundGroup = world.getBlockState(((BlockHitResult) hitResult).getBlockPos()).getSoundGroup();
+
+                world.playSound(null, hitPos.x,hitPos.y,hitPos.z, blockSoundGroup.getBreakSound(), SoundCategory.NEUTRAL, 1F, blockSoundGroup.getPitch());
+                world.syncWorldEvent(user, 2001, ((BlockHitResult) hitResult).getBlockPos(), Block.getRawIdFromState(world.getBlockState(((BlockHitResult) hitResult).getBlockPos())));
+
+                System.out.println(hitPos.x);
                 stack.getOrCreateNbt().putDouble("hitPosX", hitPos.x);
                 stack.getOrCreateNbt().putDouble("hitPosY", hitPos.y);
                 stack.getOrCreateNbt().putDouble("hitPosZ", hitPos.z);
 
+                List<Long> posXList = new ArrayList<>();
+                List<Long> posYList = new ArrayList<>();
+                List<Long> posZList = new ArrayList<>();
+                
+                
+                posXList.add(Double.doubleToLongBits(hitPos.x));
+                posYList.add(Double.doubleToLongBits(hitPos.y));
+                posZList.add(Double.doubleToLongBits(hitPos.z));
+
+                stack.getOrCreateNbt().putLongArray("xList", posXList);
+                stack.getOrCreateNbt().putLongArray("yList", posYList);
+                stack.getOrCreateNbt().putLongArray("zList", posZList);
+                
                 //Gets the distance between the HitResult Position and the Players Position
                 distance = hitPos.distanceTo(user.getEyePos());
+                
 
                 stack.getOrCreateNbt().putDouble("distance", distance);
 
@@ -56,7 +87,31 @@ public class GrappleItem extends Item {
 
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+
         NbtCompound tag = stack.getOrCreateNbt();
+
+       /* int ticks = tag.getInt("noMoveTime");
+
+        if (entity.getVelocity().x == 0 && entity.getVelocity().z == 0 && entity.isOnGround()) {
+            ticks++;
+            stack.getOrCreateNbt().putInt("noMoveTime", ticks);
+        } else {
+            stack.getOrCreateNbt().putInt("noMoveTime", 0);
+        }
+
+        if (ticks >= 120) {
+            entity.setOnFire(true);
+        }*/
+
+        long[] xList = tag.getLongArray("xList");
+
+
+        List<Long> posXList = Arrays.asList(ArrayUtils.toObject(test));
+        List<Long> posYList = new ArrayList<>();
+        List<Long> posZList = new ArrayList<>();
+
+
+
 
         double distance = tag.getDouble("distance");
         Vec3d hitPos = new Vec3d(tag.getDouble("hitPosX"), tag.getDouble("hitPosY"), tag.getDouble("hitPosZ"));
@@ -64,7 +119,6 @@ public class GrappleItem extends Item {
         if (isHooked && !entity.isOnGround()) {
             ((PlayerEntity) entity).setNoDrag(true);
             entity.setNoGravity(true);
-
 
             Vec3d grappleVector = hitPos.subtract(entity.getEyePos());
             Vec3d horizontalCorrection  = grappleVector.normalize().multiply((.08) + (Math.abs(entity.getVelocity().y) * .08), .08, (.08) + (Math.abs(entity.getVelocity().y) * .08));
@@ -82,8 +136,8 @@ public class GrappleItem extends Item {
                 if (hitPos.distanceTo(entity.getEyePos()) > distance+.1) {
 
                     if (entity.getVelocity().y < -.08) {
-                        // entity.setVelocity(entity.getVelocity().add(new Vec3d(0, testy, 0)));
-                      // entity.setVelocity(new Vec3d(entity.getVelocity().x, -testy, entity.getVelocity().z));
+                         entity.setVelocity(entity.getVelocity().add(new Vec3d(0, testy, 0)));
+                        // entity.setVelocity(new Vec3d(entity.getVelocity().x, -testy, entity.getVelocity().z));
                     }
                 }
 
