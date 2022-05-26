f4a77fa9730dd0036afdd4012ec4619ae6503781
Fusion Flux <c4gamer2.0@gmail.com>
2022-05-25 23:59:57 -0500

    trying to make dye stuff work

--- src/main/java/com/fusionflux/grapple/client/GrappleClientInitalizer.java
+++ src/main/java/com/fusionflux/grapple/client/GrappleClientInitalizer.java
@@ -5,8 +5,10 @@ import com.fusionflux.grapple.client.packets.GrapplePackets;
 import com.fusionflux.grapple.entity.HookRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.minecraft.client.item.UnclampedModelPredicateProvider;
+import net.minecraft.item.DyeableItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.util.TriConsumer;
@@ -18,5 +20,6 @@ public class GrappleClientInitalizer implements ClientModInitializer {
         GrapplePackets.registerPackets();
         EntityRendererRegistry.INSTANCE.register(Grapple.HOOK_POINT, HookRenderer::new);
         PredicateStuff.init(FabricModelPredicateProviderRegistry::register);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : ((DyeableItem) stack.getItem()).getColor(stack), Grapple.GRAPPLE);
     }
 }
